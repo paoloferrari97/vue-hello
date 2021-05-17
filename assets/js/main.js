8b2d3beb9f3e348a600343d91430e4671f74f1a9
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-17 13:57:20 +0200

    Svolgo il bonus (aggiungo img attraverso data)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 var app = new Vue({
     el: "#root",
     data: {
-        messaggio: "Hello Vue-JS!"
+        messaggio: "Hello Vue-JS!",
+        immagine: {
+            nome: "sedia",
+            percorso: "./assets/img/download.jpg"
+        }
     }
 });
-
-//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.

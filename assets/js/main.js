873405f6d010916cadd4d8c8feaefbd1e3c8253f
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-05-17 13:37:06 +0200

    Stampo a schermo msg in h1 attraverso i data

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue({
+    el: "#root",
+    data: {
+        messaggio: "Hello Vue-JS!"
+    }
+});
+
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
